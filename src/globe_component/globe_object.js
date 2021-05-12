c11e55c4d00d91aefbd32c29e91bb51a0cd88388
joypaul <jpaul3250@gmail.com>
2021-05-12 12:45:27 -0400

    merged

--- src/globe_component/globe_object.js
+++ src/globe_component/globe_object.js
@@ -11,28 +11,29 @@ const GlobeObject = (_) => {
    const [altitude, setAltitude] = useState(0.1);
    const [transitionDuration, setTransitionDuration] = useState(1000);
 
-   useEffect(() => {
-     // load data
-     fetch("../datasets/ne_110m_admin_0_countries.geojson")
-       .then((res) => res.json())
-       .then((countries) => {
-         setCountries(countries);
+  //  useEffect(() => {
+  //    // load data
+  //    fetch("../datasets/ne_110m_admin_0_countries.geojson")
+  //      .then((res) => res.json())
+  //      .then((countries) => {
+  //        setCountries(countries);
 
-         setTimeout(() => {
-           setTransitionDuration(4000);
-           setAltitude(() => (feat) =>
-             Math.max(0.1, Math.sqrt(+feat.properties.POP_EST) * 7e-5)
-           );
-         }, 3000);
-       });
-   }, []);
+  //        setTimeout(() => {
+  //          setTransitionDuration(4000);
+  //          setAltitude(() => (feat) =>
+  //            Math.max(0.1, Math.sqrt(+feat.properties.POP_EST) * 7e-5)
+  //          );
+  //        }, 3000);
+  //      });
+  //  }, []);
 
    useEffect(() => {
      // Auto-rotate
-     globeEl.current.controls().autoRotate = false;
+     globeEl.current.controls().autoRotate = true;
      globeEl.current.controls().autoRotateSpeed = 0.9;
 
-     globeEl.current.pointOfView({lat:-33.865143, lng:151.209900, altitude: 0.5 }, 5000);
+     //use this to zoom into location
+    //  globeEl.current.pointOfView({lat:-33.865143, lng:151.209900, altitude: 0.5 }, 5000);
    }, []);
 
 
