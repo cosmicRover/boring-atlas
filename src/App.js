de9add518ca5408f37d57ee84991e427bc353c4b
joypaul <jpaul3250@gmail.com>
2021-05-18 13:48:09 -0400

    zoom and marker working

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState, useRef, useEffect} from "react";
+import React, { useState, useRef, useEffect } from "react";
 import "./App.css";
 import GlobeObject from "./globe_component/globe_object";
 import CustomizedTimeline from "./timeline_components/customized_timeline";
@@ -8,7 +8,10 @@ import logo from "./assets/logo.svg";
 
 const App = () => {
   const { height, width } = useWindowDimensions();
-  const [eventItems, setEventItem] = useState([])
+  const [eventItems, setEventItem] = useState([]);
+
+  const [mapMarkers, setMapMarkers] = useState([]);
+
   const globeEl = useRef();
 
   useEffect(() => {
@@ -16,46 +19,83 @@ const App = () => {
     globeEl.current.controls().autoRotate = true;
     globeEl.current.controls().autoRotateSpeed = 0.1;
 
-    //use this to zoom into location 
+    fetchEvents("3000BCE");
+
+    //use this to zoom into location
   }, []);
 
+  function handleOnPressed(index) {
+    var item = eventItems[index];
+    focusOnEvent(item.lat, item.lon, 0.5);
+  }
+
+  function handleOnLabelClicked(item) {
+    console.log(item);
+  }
+
   //zooms in to a particular lat, lon on the globe
-  function focusOnEvent(lat, lon, alt){
-    globeEl.current.pointOfView(
-      { lat: lat, lng: lon, altitude: alt },
-      5000
-    );
+  function focusOnEvent(lat, lon, alt) {
+    globeEl.current.pointOfView({ lat: lat, lng: lon, altitude: alt }, 5000);
     globeEl.current.controls().autoRotate = false;
   }
 
+  //helper fucntion to plot the data
+
   function fetchEvents(era) {
     console.log(era);
+    setEventItem([]);
 
     const requestOptions = {
       method: "POST",
       // headers: { "Content-Type": "application/json"},
       body: JSON.stringify({
-        era: "3100BCE", //get era from the slider
+        era: era, //get era from the slider
       }),
     };
 
     fetch(
       "https://us-east-1.aws.webhooks.mongodb-realm.com/api/client/v2.0/app/boring-atlas-rest-api-hqzzz/service/boring-atlas-data-endpoint/incoming_webhook/boring-atlas-endpoint-webhook",
       requestOptions
-    )
-      .then((response) => response.json())
-      .then(function (data) {
-        const items = data[0]["events"];
+    ).then(function (response) {
+      if (response.status === 200) {
+        response.json().then(function (data) {
+          const items = data[0]["events"];
 
-        //add these do data using hooks
-        items.forEach((ele, _) => {
-          var items = ele.split(";");
-          console.log(items);
+          // console.log(items)
+
+          //add these do data using hooks
+          items.forEach((ele, index) => {
+            var events = ele.split(";");
+
+            const coordinates = events[4].split(",");
+            coordinates[0].trim();
+            coordinates[1].trim();
+            const lat = parseFloat(coordinates[0]);
+            const lon = parseFloat(coordinates[1]);
+
+            // console.log(events.toString());
+
+            const eventObject = {
+              date: events[0].trim(),
+              title: events[1].trim(),
+              event: events[2].trim(),
+              type: events[3].trim(),
+              lat: lat,
+              lon: lon,
+            };
+
+            //plot the data
+
+            console.log("event items are ->>>> ", eventObject);
+
+            setEventItem((oldEvents) => [...oldEvents, eventObject]);
+          });
         });
-
-        //pass lat, lon from the json along with an altitude to zoom in to the map
-        focusOnEvent(-33.865143, 151.2099, 0.5)
-      });
+      } else {
+        console.log("No events found, error!");
+        setEventItem([]);
+      }
+    });
   }
 
   var timelinePositionStyle = {
@@ -80,11 +120,15 @@ const App = () => {
 
   return (
     <div>
-      <GlobeObject globeEl={globeEl}/>
+      <GlobeObject
+        globeEl={globeEl}
+        data={eventItems}
+        hadleLabelClick={(e) => handleOnLabelClicked(e)}
+      />
       <div style={timelinePositionStyle}>
         <CustomizedTimeline
-          data={[1, 2, 3, 4, 5, 6, 7, 8, 9]}
-          onClicked={(e) => fetchEvents(e)}
+          data={eventItems}
+          onClicked={(index) => handleOnPressed(index)}
         />
       </div>
       <div style={sliderPositionStyle}>
--- src/globe_component/globe_object.js
+++ src/globe_component/globe_object.js
@@ -3,44 +3,31 @@ import {React, useRef, useState, useEffect} from "react";
 import "../App.css";
 import useWindowDimensions from "../services/useWindowDimensions";
 
-const GlobeObject = ({globeEl}) => {
-   
-  //TODO clean these up. might need to pass globeEl from parent class
-  
-   const [countries, setCountries] = useState({ features: [] });
-   const [altitude, setAltitude] = useState(0.1);
-   const [transitionDuration, setTransitionDuration] = useState(1000);
-
-  //  useEffect(() => {
-  //    // load data
-  //    fetch("../datasets/ne_110m_admin_0_countries.geojson")
-  //      .then((res) => res.json())
-  //      .then((countries) => {
-  //        setCountries(countries);
-
-  //        setTimeout(() => {
-  //          setTransitionDuration(4000);
-  //          setAltitude(() => (feat) =>
-  //            Math.max(0.1, Math.sqrt(+feat.properties.POP_EST) * 7e-5)
-  //          );
-  //        }, 3000);
-  //      });
-  //  }, []);
-
-
-  //pass params
+const GlobeObject = ({globeEl, data, hadleLabelClick}) => {
   const { height, width } = useWindowDimensions();
 
 
   return (
     <Globe
-    ref={globeEl}
+      ref={globeEl}
       class="globe-container"
       globeImageUrl="https://res.cloudinary.com/dmhit0ltx/image/upload/v1613871735/3_no_ice_clouds_8k_cnbuqw.jpg"
       bumpImageUrl="//unpkg.com/three-globe/example/img/earth-topology.png"
       backgroundImageUrl="https://res.cloudinary.com/dmhit0ltx/image/upload/v1620417966/wow_m8riir.jpg"
       height={height}
       width={width}
+      // labelsData={data}
+
+      labelsData={data}
+      labelLat="lat"
+      labelLng="lon"
+      labelDotRadius={1}
+      labelText="title"
+      labelDotOrientation={() => "bottom"}
+      labelSize={1}
+      labelColor={() => "rgb(255, 0, 0, 0.5)"}
+      labelResolution={2}
+      onLabelClick={(item) => hadleLabelClick(item)} //todo pass an unique key to the callback
     />
   );
 };
