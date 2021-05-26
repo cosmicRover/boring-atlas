f438bdabb28d73b813c685ffeb2ec9cb085abe68
joypaul <jpaul3250@gmail.com>
2021-05-26 14:23:31 -0400

    semi-final

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-
 import React, { useState, useRef, useEffect } from "react";
 import "./App.css";
 import GlobeObject from "./globe_component/globe_object";
@@ -10,6 +9,8 @@ import Popper from "./popup/popper";
 import "react-sliding-pane/dist/react-sliding-pane.css";
 import { makeStyles } from "@material-ui/core/styles";
 import Drawer from "@material-ui/core/Drawer";
+import Typography from "@material-ui/core/Typography";
+import Box from "@material-ui/core/Box";
 
 const useStyles = makeStyles({
   paper: {
@@ -20,14 +21,13 @@ const useStyles = makeStyles({
   },
 });
 
-import React, { useState, useRef, useEffect } from 'react';
-import './App.css';
-import GlobeObject from './globe_component/globe_object';
-import CustomizedTimeline from './timeline_components/customized_timeline';
-import DiscreteSlider from './slider/discrete_slider';
-import useWindowDimensions from './services/useWindowDimensions';
-import logo from './assets/ba_logo.png';
-
+// import React, { useState, useRef, useEffect } from 'react';
+// import './App.css';
+// import GlobeObject from './globe_component/globe_object';
+// import CustomizedTimeline from './timeline_components/customized_timeline';
+// import DiscreteSlider from './slider/discrete_slider';
+// import useWindowDimensions from './services/useWindowDimensions';
+// import logo from './assets/ba_logo.png';
 
 const App = () => {
   const { height, width } = useWindowDimensions();
@@ -41,7 +41,6 @@ const App = () => {
     globeEl.current.controls().autoRotateSpeed = 0.1;
 
     fetchEvents("3000BCE");
-
   }, []);
 
   function handleOnPressed(index) {
@@ -70,8 +69,6 @@ const App = () => {
   }
 
   //this is for slide-in window on the right side
-
-  //state and function for popup window
   const classes = useStyles();
   const [isOpen, setIsOpen] = useState(false);
   const toggleDrawer = (open) => (event) => {
@@ -96,12 +93,16 @@ const App = () => {
   }
 
   //helper fucntion to plot the data
+  const [yearLabelText, setYearLabelText] = useState("3000BCE");
+
   function fetchEvents(era) {
     console.log(`fetching era: ${era}`);
+    setYearLabelText(era);
+
     setEventItem([]);
 
     const requestOptions = {
-      method: 'POST',
+      method: "POST",
       // headers: { "Content-Type": "application/json"},
       body: JSON.stringify({
         era: era, //get era from the slider
@@ -109,7 +110,7 @@ const App = () => {
     };
 
     fetch(
-      'https://us-east-1.aws.webhooks.mongodb-realm.com/api/client/v2.0/app/boring-atlas-rest-api-hqzzz/service/boring-atlas-data-endpoint/incoming_webhook/boring-atlas-endpoint-webhook',
+      "https://us-east-1.aws.webhooks.mongodb-realm.com/api/client/v2.0/app/boring-atlas-rest-api-hqzzz/service/boring-atlas-data-endpoint/incoming_webhook/boring-atlas-endpoint-webhook",
       requestOptions
     ).then(function (response) {
       if (response.status === 200) {
@@ -120,15 +121,15 @@ const App = () => {
             return;
           }
 
-          const items = data[0]['events'];
+          const items = data[0]["events"];
 
           // console.log(items)
 
           //add these do data using hooks
           items.forEach((ele, index) => {
-            var events = ele.split(';');
+            var events = ele.split(";");
 
-            const coordinates = events[4].split(',');
+            const coordinates = events[4].split(",");
             coordinates[0].trim();
             coordinates[1].trim();
             const lat = parseFloat(coordinates[0]);
@@ -147,13 +148,13 @@ const App = () => {
 
             //plot the data
 
-            console.log('event items are ->>>> ', eventObject);
+            console.log("event items are ->>>> ", eventObject);
 
             setEventItem((oldEvents) => [...oldEvents, eventObject]);
           });
         });
       } else {
-        console.log('No events found, error!');
+        console.log("No events found, error!");
         setEventItem([]);
       }
     });
@@ -164,20 +165,32 @@ const App = () => {
     // backgroundColor: 'RGBA(20, 39, 59, 0.5)',
     height: `${(height - 228).toString()}px`,
     width: `${100}`,
-    position: 'absolute',
-    top: '128px',
-    left: '28px',
-    overflow: 'auto',
+    position: "absolute",
+    top: "128px",
+    left: "28px",
+    overflow: "auto",
   };
 
   var sliderPositionStyle = {
     // backgroundColor: 'RGBA(20, 39, 59, 0.5)',
     height: `100px`,
     width: `${100}`,
-    position: 'absolute',
-    bottom: '28px',
-    left: '130px',
-    right: '130px',
+    position: "absolute",
+    bottom: "28px",
+    left: "130px",
+    right: "130px",
+  };
+
+  var labelPositionStyle = {
+    // backgroundColor: 'RGBA(20, 39, 59, 0.5)',
+    height: `100px`,
+    width: `${100}`,
+    position: "absolute",
+    top: "25px",
+    left: `${((width-350)/2).toString()}px`,
+    // bottom: "28px",
+    // left: "130px",
+    // right: "130px",
   };
 
   return (
@@ -189,13 +202,26 @@ const App = () => {
       />
 
       {/* Logo */}
+
       <a href="/" id="logo">
         <img src={logo} alt="logo" />
       </a>
 
-      {/* Year Displayer */}
-      <div id="year">
-        <h1>Current Year: 3000 BC</h1>
+      <div style={labelPositionStyle}>
+        {/* Year Displayer */}
+        <Typography component="div">
+          <Box
+            fontWeight="fontWeightLight"
+            fontFamily="Montserrat"
+            fontSize="34px"
+            m={1}
+            lineHeight={0.95}
+            color="RGBA(243, 163, 17, 1)"
+            textAlign="center"
+          >
+            {`Current Year: ${yearLabelText}`}
+          </Box>
+        </Typography>
       </div>
 
       <div style={timelinePositionStyle}>
@@ -208,7 +234,6 @@ const App = () => {
         <DiscreteSlider getSliderValue={(era) => fetchEvents(era)} />
       </div>
 
-
       <div>
         <Drawer
           open={isOpen}
@@ -219,7 +244,6 @@ const App = () => {
           <Popper value={eventItems[focusIndex]} />
         </Drawer>
       </div>
-
     </div>
   );
 };
